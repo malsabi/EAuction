bc34cfebe82280ddf2907813b82f589ae8af6849
U17101952 <U17101952@sharjah.ac.ae>
2021-08-13 15:43:28 +0400

    Login Screen Frontend First Design

--- app/src/main/java/com/example/eauction/LoginActivity.java
+++ app/src/main/java/com/example/eauction/LoginActivity.java
@@ -3,14 +3,26 @@ package com.example.eauction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.royrodriguez.transitionbutton.TransitionButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TransitionButton transitionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Abdelhady was here
+        transitionButton = findViewById(R.id.transition_button);
+        transitionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transitionButton.startAnimation();
+            }
+        });
+
     }
 }
