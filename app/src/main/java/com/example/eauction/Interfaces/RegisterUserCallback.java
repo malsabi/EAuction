3e07877f338bd803a4d06f49e076b59991df0b16
U16106818@sharjah.ac.ae <U16106818@sharjah.ac.ae>
2021-08-21 17:55:52 +0400

    Added firebasefireStore to the project, linked sign in / sign up.

---
+++ app/src/main/java/com/example/eauction/Interfaces/RegisterUserCallback.java
@@ -0,0 +1,8 @@
+package com.example.eauction.Interfaces;
+
+import com.example.eauction.Models.FireStoreResult;
+
+public interface RegisterUserCallback
+{
+    void onCallback(FireStoreResult Result);
+}
---
+++ app/src/main/java/com/example/eauction/Interfaces/SignInUserCallback.java
@@ -0,0 +1,8 @@
+package com.example.eauction.Interfaces;
+
+import com.example.eauction.Models.FireStoreResult;
+
+public interface SignInUserCallback
+{
+    void onCallback(FireStoreResult Result);
+}
