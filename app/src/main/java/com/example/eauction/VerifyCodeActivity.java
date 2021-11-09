66a49bf2632885ee7d4ba0ef7d65552da112db71
U17101952 <U17101952@sharjah.ac.ae>
2021-11-09 07:54:37 +0400

    Timer, Timer ending event, code. TODO: Testing

--- app/src/main/java/com/example/eauction/VerifyCodeActivity.java
+++ app/src/main/java/com/example/eauction/VerifyCodeActivity.java
@@ -2,15 +2,19 @@ package com.example.eauction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.QuickContactBadge;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.eauction.Helpers.ResetPasswordHelper;
+import com.github.gongw.VerifyCodeView;
 import com.royrodriguez.transitionbutton.TransitionButton;
 
 import butterknife.BindView;
@@ -19,10 +23,13 @@ import butterknife.ButterKnife;
 public class VerifyCodeActivity extends AppCompatActivity
 {
     @BindView(R.id.EtOTPCode)
-    EditText OTPCodeEditText;
+    VerifyCodeView OTPCodeEditText;
 
     @BindView(R.id.VerifyCodeBtn)
     TransitionButton VerifyCodeButton;
+
+    @BindView(R.id.EtTimer)
+    TextView EtTimer;
 
     private App AppInstance;
     private String OTPCode;
@@ -49,7 +56,8 @@ public class VerifyCodeActivity extends AppCompatActivity
 
         ResetPasswordHelper.SendMessage(CompanyAccount, CompanyPassword, Email, "OTP CODE", "Your OTP Code is: " + OTPCode);
 
-        VerifyCodeButton.setOnClickListener(view -> HandleVerifyCode(OTPCodeEditText.getText().toString()));
+        VerifyCodeButton.setOnClickListener(view -> HandleVerifyCode(OTPCodeEditText.getVcText().toString()));
+        startTimer(60000, 1000);
     }
 
     private void HandleVerifyCode(String UserCode)
@@ -57,7 +65,8 @@ public class VerifyCodeActivity extends AppCompatActivity
         VerifyCodeButton.startAnimation();
         if (UserCode == null || UserCode.length() == 0)
         {
-            OTPCodeEditText.setError("OTP Code cannot be empty, please insert a valid OTP Code.");
+            //OTPCodeEditText.setError("OTP Code cannot be empty, please insert a valid OTP Code.");
+            Toast.makeText(this, "OTP Code cannot be empty, please insert a valid OTP Code.", Toast.LENGTH_SHORT).show();
             VerifyCodeButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
         }
         else
@@ -72,9 +81,27 @@ public class VerifyCodeActivity extends AppCompatActivity
             }
             else
             {
-                OTPCodeEditText.setError("Invalid Code, please insert a valid OTP Code.");
+                //OTPCodeEditText.setError("Invalid Code, please insert a valid OTP Code.");
+                Toast.makeText(this, "Invalid Code, please insert a valid OTP Code.", Toast.LENGTH_SHORT).show();
                 VerifyCodeButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
             }
         }
+    }
+    //Helper Timer Button
+    private void startTimer(final long finish, long tick) {
+        CountDownTimer t;
+        t = new CountDownTimer(finish, tick) {
+
+            @SuppressLint("SetTextI18n")
+            public void onTick(long millisUntilFinished) {
+                String remainedSecs = String.valueOf(millisUntilFinished / 1000);
+                EtTimer.setText("Code expires in "+remainedSecs+" seconds");
+            }
+
+            public void onFinish() { //TODO This is the event for the timer
+                Toast.makeText(VerifyCodeActivity.this, "Timer is over", Toast.LENGTH_SHORT).show();
+                cancel();
+            }
+        }.start();
     }
 }
